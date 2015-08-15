96ef5d1d4d3147e21fe73a98c037fc7e4e5700a2
Nikhil Jain <blockerdude2@yahoo.com>
2015-08-15 13:12:47 -0500

    Captured new player entries on new game setup
    Have individual handle on each inflated view.

--- GolfStats/app/src/main/java/edu/jain/nikhil/golfstats/NewGameSetup.java
+++ GolfStats/app/src/main/java/edu/jain/nikhil/golfstats/NewGameSetup.java
@@ -3,26 +3,43 @@ package edu.jain.nikhil.golfstats;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 
 public class NewGameSetup extends Activity {
 
+    private ArrayList<View> players;
+    private int text =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game_setup);
+        players = new ArrayList<View>();
+        addPlayer(this.findViewById(android.R.id.content).getRootView());
     }
 
     public void addPlayer(View view){
         LinearLayout playerLayout = (LinearLayout) findViewById(R.id.LinearLayout_NewGameSetup_PlayerEntry);
         View playerEntry = View.inflate(this, R.layout.add_player_element, playerLayout);
-       // playerLayout.addView(playerEntry);
+        //EditText etext = (EditText)playerEntry.findViewById(R.id.EditText_AddPlayerElement_PlayerName);
+        //etext.setText("BUtts"+text);
+        players.add(playerLayout.getChildAt(text).findViewById(R.id.EditText_AddPlayerElement_PlayerName));
+        EditText eText = (EditText)players.get(text).findViewById(R.id.EditText_AddPlayerElement_PlayerName);
+        eText.setText(""+text);
+        text++;
+       // players.add(playerEntry);
+
 
     }
 
     public void startGame(View view){
-
+        for(int x=0; x<players.size(); x++){
+            EditText editText = (EditText)players.get(x).findViewById(R.id.EditText_AddPlayerElement_PlayerName);
+            editText.setText("WORKS" + x);
+        }
     }
 
 }
